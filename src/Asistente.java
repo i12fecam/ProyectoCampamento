2743fc4f27b5b904597bde5bcb2a39530a0aff5f
i12rumon <i12rumon@uco.es>
2023-10-03 12:07:11 +0200

    Añadir fecha de nacimiento para Asistente.java con getters y setters y constructores

--- src/Asistente.java
+++ src/Asistente.java
@@ -2,12 +2,20 @@ import java.time.LocalDate;
 public class Asistente extends Persona{
     @Override
     private LocalDate fecha_nacimiento;
+    public Asistente(){
+
+    }
+    public Asistente(int identificador, String nombre, String apellido1, String apellido2, boolean especial, LocalDate fechaNacimiento) {
+        super(identificador, nombre, apellido1, apellido2, especial);
+        this.fechaNacimiento = fechaNacimiento;
+    }
     public String toString() {
         return "Asistente{" +
                 "identificador=" + getIdentificador() +
                 ", nombre='" + getNombre() + '\'' +
                 ", apellido1='" + getApellido1() + '\'' +
                 ", apellido2='" + getApellido2() + '\'' +
+                ", Fecha de Nacimiento:'" + getFecha_nacimiento() + '\'' +
                 ", especial=" + isEspecial() +
                 '}';
     }
---
+++ src/Campamento.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+public class Campamento{
+    private int idCampamento;
+    private LocalDate fecha_inicio;
+    private LocalDate fecha_final;
+
+}
