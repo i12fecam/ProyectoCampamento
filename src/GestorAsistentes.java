4a2af03a9d4dd0f41b1b8988b870896f876bc651
i12qugaa <i12qugaa@uco.es>
2023-10-11 13:19:55 +0200

    Modificacion clase Gestor asistente

--- src/GestorAsistentes.java
+++ src/GestorAsistentes.java
@@ -10,15 +10,32 @@ public class GestorAsistentes {
     public GestorAsistentes() {
     }
 
-    public boolean darAlta(Asistente asistente){
-        if(asistentes.contains(asistente)){
-            return false;
+    public boolean darAlta(Asistente asistente) {
+        if (asistentes.contains(asistente)) {
+            throw new IllegalArgumentException("El asistente ya esta dado de alta");
+
         }
         asistentes.add(asistente);
         return true;
     }
     //modificarAsistentes , hacemos una funcion por cada atributo a modificar?
-    public void listar(){
-
+    public void listar() {
+        String valor;
+        for (Asistente asistente : asistentes) {
+            if(asistente.isAtencionEspecial()){
+                valor="true";
+            }else{
+                valor="false";
+            }
+            System.out.println ("Asistente{" +
+                    "identificador=" + asistente.getIdentificador() +
+                    ", nombre='" + asistente.getNombre() + '\'' +
+                    ", apellido1='" + asistente.getApellido1() + '\'' +
+                    ", apellido2='" + asistente.getApellido2() + '\'' +
+                    ", Fecha de Nacimiento:'" + asistente.getFechaNacimiento() + '\'' +
+                    ",especial=" + valor +
+                    '}');
+        }
     }
+
 }
