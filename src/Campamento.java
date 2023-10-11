84fe86a413cd0e13015b91b18b159218409585fa
Abigail Fernández <i12fecam@uco.es>
2023-10-11 14:28:57 +0200

    añadido comparaciones, preguntas en el main e incio de GestorCampamentos

--- src/Campamento.java
+++ src/Campamento.java
@@ -33,6 +33,9 @@ public class Campamento{
      * @param maxAsistentes numero maximo de asistentes del campamento
      */
     public Campamento(int idCampamento, LocalDate fechaInicio, LocalDate fechaFinal, NivelEducativo nivelEducativo, int maxAsistentes) {
+        if(fechaFinal.isAfter(fechaInicio)){
+            throw new RuntimeException("Las fechas no tienen sentido");
+        }
         this.idCampamento = idCampamento;
         this.fechaInicio = fechaInicio;
         this.fechaFinal = fechaFinal;
@@ -210,5 +213,9 @@ public class Campamento{
         } else {
             throw new RuntimeException("El monitor ya está asociado o no es educador especial");
         }
+    }
+    public boolean equals (Campamento campamento) {
+        return campamento.idCampamento == this.idCampamento;
+
     }
 }
--- src/GestorCampamentos.java
+++ src/GestorCampamentos.java
@@ -1,16 +1,37 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class GestorCampamentos {
     private ArrayList<Campamento> campamentos;
 
-
+    private ArrayList<Monitor> monitores;
+    private ArrayList<Actividad> actividades;
     private void cargarFichero(){
 
     }
-    public boolean crearCampamento(){
-    return false;
+    public void crearCampamento(int idCampamento, LocalDate fechaInicio, LocalDate fechaFinal, NivelEducativo nivelEducativo, int maxAsistentes){
+        Campamento campamento = new Campamento(idCampamento,fechaInicio,fechaFinal,nivelEducativo,maxAsistentes);
+        if(campamentos.contains(campamento)){
+            throw new RuntimeException("El campamento ya existia");
+        }
+        campamentos.add( new Campamento(idCampamento,fechaInicio,fechaFinal,nivelEducativo,maxAsistentes));
     }
 
+    public void crearMonitor(int identificador, String nombre, String apellido1, String apellido2, LocalDate fechaNacimiento, boolean educadorEspecial){
+        Monitor monitor = new Monitor(identificador,nombre,apellido1,apellido2,fechaNacimiento,educadorEspecial);
+        if(monitores.contains(monitor)){
+            throw  new RuntimeException("El monitor ya existía");
+        }
+        monitores.add(monitor);
+    }
+
+    public void crearActividad(String nombre, int identificador, NivelEducativo nivelEducativo, Horario horario, int maxParticipantes, int monitoresNecesarios){
+        Actividad actividad = new Actividad(nombre,identificador,nivelEducativo,horario,maxParticipantes,monitoresNecesarios);
+        if(actividades.contains(actividad)){
+            throw new RuntimeException("la actividad ya existía");
+        }
+        actividades.add(actividad);
+    }
     public boolean asociarMonitorActividad(){
         return false;
     }
