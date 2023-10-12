51f4e53ae7c434acec3e41fb0c705be514c98916
Abigail Fernández <i12fecam@uco.es>
2023-10-12 17:29:57 +0200

    limpiado cosas

--- src/InscripcionFactory.java
+++ src/InscripcionFactory.java
@@ -2,11 +2,7 @@ import java.time.LocalDate;
 
 public abstract class InscripcionFactory {
 
-    protected int idParticipante;
-    protected int idCampamento;
-    protected LocalDate fechaInscripcion;
-    protected float precio;
-    protected TipoInscripcion tipoInscripcion;
+
     public abstract InscripcionCompleta crearInscripcionCompleta(int idParticipante, int idCampamento, LocalDate fechaInscripcion, float precio);
 
     public abstract InscripcionParcial crearInscripcionParcial(int idParticipante, int idCampamento, LocalDate fechaInscripcion, float precio);
