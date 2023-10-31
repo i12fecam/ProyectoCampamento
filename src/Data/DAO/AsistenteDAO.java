76df20b4b0af82ce7a2560f8c3ee56332ba22d03
i12qugaa <i12qugaa@uco.es>
2023-10-31 19:44:04 +0100

    Modificacion

--- src/Data/DAO/AsistenteDAO.java
+++ src/Data/DAO/AsistenteDAO.java
@@ -1,16 +1,49 @@
 package Data.DAO;
 
 import Data.DAO.Common.ConexionBD;
+import Data.DAO.Common.ProyectProperties;
 import Data.DTO.Asistente;
 
 import java.sql.Connection;
 import java.time.LocalDate;
 
+import java.sql.*;
+
+
 public class AsistenteDAO {
 
+
+    private ProyectProperties prop;
+
+    private ConexionBD bd;
+
+    private Connection con;
+
+    public AsistenteDAO(){
+        prop = new ProyectProperties();
+        bd = new ConexionBD();
+
+        con = bd.getConnection(prop.getUrl(), prop.getUsername(), prop.getPassword());
+    }
     public void crear(Asistente asistente){
-        ConexionBD conexionBD = new ConexionBD();
-        //Connection con = conexionBD.getConnection();
+        try {
+            PreparedStatement ps = con.prepareStatement(prop.getSentente("insert_record_Asistentes"));
+
+            ps.setInt(1, asistente.getIdentificador());
+            ps.setString(2, asistente.getNombre());
+            ps.setDate(3, Date.valueOf(asistente.getFechaNacimiento()));
+            ps.setInt(4, asistente.isAtencionEspecial() ? 1 : 0);
+            ps.setString(5, asistente.getApellido1() + " " + asistente.getApellido2()); // ajusta para obtener el valor correcto
+
+            int status = ps.executeUpdate();
+            if (status > 0) {
+                System.out.println("Asistente agregado con éxito");
+            } else {
+                System.out.println("Fallo al agregar el asistente");
+            }
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
